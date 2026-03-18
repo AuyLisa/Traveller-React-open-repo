18bf4af6256a0df888bf607c58fae14147227d80
AuyLisa <166059787+AuyLisa@users.noreply.github.com>
2026-03-18 05:24:28 +0300

    Update AviaCard.jsx

--- src/components/avia-card/AviaCard.jsx
+++ src/components/avia-card/AviaCard.jsx
@@ -2,13 +2,23 @@ import './AviaCard.css';
 
 function AviaCard( {avia} ) {
   return (
-    <div className="avia-card">
-      <h3>{avia.from} → {avia.to}</h3>
-      <p>{avia.departure} → {avia.arrival}</p>
-      <p className="price">{avia.price} ₽</p>
-      <p>{avia.airline}</p>
-      <p>{avia.duration}</p>
-      <button>Подробнее</button>
+    <div className="aviacard">
+
+      <div className="aviacard__title">
+        <p className="aviacard__from">{avia.from}</p>
+        <p className="aviacard__to">{avia.to}</p>
+      </div>
+
+      <div className="aviacard__timetable">
+        <p className="aviacard__departure">{avia.departure}</p>
+        <p className="aviacard__arrival">{avia.arrival}</p>
+      </div>
+
+      <p className="aviacard__duration"> В пути: {avia.duration}</p>
+      <p className="aviacard__airline">{avia.airline}</p>
+      {/*<p className="aviacard__price">{avia.price} ₽</p>
+      цены сделать эконом комфорт бизнес*/}
+      <button className="aviacard__button">Подробнее</button>
     </div>
   );
 }
