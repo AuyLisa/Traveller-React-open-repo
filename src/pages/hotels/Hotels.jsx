b4368bda2765799a27eaf552ca03eb47ece8a236
AuyLisa <166059787+AuyLisa@users.noreply.github.com>
2026-03-18 05:31:49 +0300

    Update Hotels.jsx

--- src/pages/hotels/Hotels.jsx
+++ src/pages/hotels/Hotels.jsx
@@ -10,16 +10,51 @@
 import Layout from '../../components/layout/Layout';
 import HotelCard from '../../components/hotel-card/HotelCard';
 import hotels from '../../data/hotels'; 
+import './Hotels.css';
 import {useState} from 'react';
 
 
+const countries = [
+  { value: '', label: 'все страны'},
+  { value: 'Турция', label: 'Турция'},
+  { value: 'ОАЭ', label: 'ОАЭ'},
+  { value: 'Египет', label: 'Египет'},
+  { value: 'Таиланд', label: 'Таиланд'},
+  { value: 'Грузия', label: 'Грузия'},
+  { value: 'Мальдивы', label: 'Мальдивы'}
+];
+
+const stars = [
+  { value: 0, label: 'все отели'},
+  { value: 5, label: '⭐⭐⭐⭐⭐'},
+  { value: 4, label: '⭐⭐⭐⭐'},
+  { value: 3, label: '⭐⭐⭐'},
+  { value: 2, label: '⭐⭐'},
+  { value: 1, label: '⭐'},
+];
+
+const reviews = [
+  { value: 0, label: 'любое число отзывов'},
+  { value: 20, label: '20+ отзывов'},
+  { value: 50, label: '50+ отзывов'},
+  { value: 100, label: '100+ отзывов'},
+  { value: 200, label: '200+ отзывов'},
+  { value: 500, label: '500+ отзывов'}
+]
+
+const prices = [
+  { value: '', label: 'все цены'},
+  { value: 'asc', label: 'сначала дешевые'},
+  { value: 'desc', label: 'сначала дорогие'}
+]
+
 function Hotels() {
 
-  const [chosenCountry, setCountry] = useState('');  // по умолч=все страны строка
-  const [chosenStars, setStars] = useState(0);  // по умолч=все звезды число (в бд)
+  const [chosenCountry, setCountry] = useState('');
+  const [chosenStars, setStars] = useState(0);
   //const [chosenOptions, setOptions] = useState([]); // -
-  const [minReviews, setminReviews] = useState(0); // по числу отзывов по умолч=все
-  const [chosenSortPrice, setSortPrice] = useState(''); //по умолч без сортировки
+  const [minReviews, setminReviews] = useState(0);
+  const [chosenSortPrice, setSortPrice] = useState('');
 
 
 
@@ -31,7 +66,7 @@ function Hotels() {
   //по звездам
   const s_filtered = chosenStars === 0
   ? c_filtered  //результат после фильтра по странам
-  : c_filtered.filter(hotel => hotel.stars === chosenStars);
+  : c_filtered.filter(hotel => hotel.star === chosenStars);
 
   //по опциям пока -
   {/*const op_filtered = chosenOptions.length === 0
@@ -44,7 +79,7 @@ function Hotels() {
   //по числу отзывов
   const r_filtered = minReviews === 0
   ? s_filtered 
-  : s_filtered.filter(hotel => hotel.reviews >= minReviews);
+  : s_filtered.filter(hotel => hotel.review >= minReviews);
 
 
   //по цене
@@ -62,51 +97,54 @@ function Hotels() {
 
   return (
     <Layout>
-      <h1>Отели</h1>
-      <div style={{ display: 'flex', gap: '20px', margin: '20px 0' }}>
+      <h1 className="hotels__title">Отели</h1>
+      <div className="hotels__filters">
 
         {/* по странам */}
-        <select value={chosenCountry} onChange={(e) => setCountry(e.target.value)}>
-          <option value="">все страны</option>
-          <option value="Турция"> Турция </option>
-          <option value="ОАЭ"> ОАЭ </option>
-          <option value="Египет"> Египет </option>
-          <option value="Таиланд"> Таиланд </option>
-          <option value="Грузия"> Грузия </option>
-          <option value="Мальдивы"> Мальдивы </option>
+        <select className="hotels__select"
+        value={chosenCountry} onChange={(e) => setCountry(e.target.value)}>
+          {countries.map(country => (<option key={country.value} value={country.value}> 
+          {country.label}
+          </option>
+          ))}
         </select>
 
         {/* по звездам */}
-        <select value={chosenStars} onChange={(e) => setStars(Number(e.target.value))}>
-          <option value="0">все отели</option>
-          <option value="5">5 ⭐⭐⭐⭐⭐</option>
-          <option value="4">4 ⭐⭐⭐⭐</option>
-          <option value="3">3 ⭐⭐⭐</option>
-          <option value="2">2 ⭐⭐</option>
-          <option value="1">1 ⭐</option>
+        <select className="hotels__select"
+        value={chosenStars} onChange={(e) => setStars(Number(e.target.value))}>
+          {stars.map(star => (<option key={star.value} value={star.value}> 
+          {star.label}
+          </option>
+          ))}
         </select>
 
 
-      {/* по числу отзывов */}
-      <select value={minReviews} onChange={(e) => setminReviews(Number(e.target.value))}>
-          <option value="0">любое число отзывов</option>
-          <option value="20"> 20+ отзывов</option>
-          <option value="50">50+ отзывов</option>
-          <option value="100">100+ отзывов</option>
-          <option value="200">200+ отзывов</option>
-          <option value="500">500+ отзывов</option>
+        {/* по числу отзывов */}
+        <select className="hotels__select"
+        value={minReviews} onChange={(e) => setminReviews(Number(e.target.value))}>
+          {reviews.map(review => (<option key={review.value} value={review.value}> 
+            {review.label}
+            </option>
+            ))}
         </select>
 
-      {/* по цене */}
-      <select value={chosenSortPrice} onChange={(e) => setSortPrice(e.target.value)}>
-          <option value="">все цены</option>
-          <option value="asc"> сначала дешевые</option>
-          <option value="desc"> сначала дорогие</option>
+        {/* по цене */}
+        <select className="hotels__select"
+        value={chosenSortPrice} onChange={(e) => setSortPrice(e.target.value)}>
+          {prices.map(price => (<option key={price.value} value={price.value}> 
+            {price.label}
+            </option>
+            ))}
         </select>
+
+
       </div>
 
+      <div className="hotels__results">
+        Найдено отелей:  
+        <span className="hotels__count"> {res.length}</span>
+      </div>
 
-      <p>Найдено отелей: {res.length}</p>
       {res.map(hotel => (
           <HotelCard key={hotel.id} hotel={hotel} />
         ))}
