9ccd2c2015cf6b580663a051fef57827f97a013a
gren75 <green7195@yandex.ru>
2026-04-15 08:37:38 +0300

    опции select в константы

--- src/pages/profile/PersonalData.jsx
+++ src/pages/profile/PersonalData.jsx
@@ -4,6 +4,43 @@ import { useState, useEffect } from 'react';
 import { normalizePhoneDigits } from '../../utils/formValidation';
 import './Profile.css';
 
+const GENDER_OPTIONS = [
+  { value: "", label: "Не выбран" },
+  { value: "female", label: "Женский" },
+  { value: "male", label: "Мужской" }
+];
+
+const AGE_OPTIONS = [
+  { value: "", label: "Не выбран" },
+  { value: "18-30", label: "18-30 лет" },
+  { value: "31-45", label: "31-45 лет" },
+  { value: "46-60", label: "46-60 лет" },
+  { value: "60+", label: "60+ лет" }
+];
+
+const COUNTRY_OPTIONS = [
+  { value: "", label: "Не выбрана" },
+  { value: "ru", label: "Россия" },
+  { value: "kz", label: "Казахстан" },
+  { value: "by", label: "Беларусь" },
+  { value: "ge", label: "Грузия" },
+  { value: "tr", label: "Турция" }
+];
+
+const LANGUAGE_OPTIONS = [
+  { value: "", label: "Не выбран" },
+  { value: "ru", label: "Русский" },
+  { value: "en", label: "English" }
+];
+
+const CURRENCY_OPTIONS = [
+  { value: "RUB", label: "₽ Рубль" },
+  { value: "USD", label: "$ Доллар" },
+  { value: "EUR", label: "€ Евро" },
+  { value: "TRY", label: "₺ Лира" }
+];
+
+
 
 function PersonalData() {
   
@@ -58,9 +95,11 @@ function PersonalData() {
               onChange={(e) => setGender(e.target.value)}
               className="profile__select"
             >
-              <option value="">Не выбран</option>
-              <option value="female">Женский</option>
-              <option value="male">Мужской</option>
+            {GENDER_OPTIONS.map(option => (
+              <option key={option.value} value={option.label}>
+                {option.label}
+              </option>
+            ))}
             </select>
           </div>
 
@@ -73,11 +112,11 @@ function PersonalData() {
               onChange={(e) => setAge(e.target.value)}
               className="profile__select"
             >
-              <option value="">Не выбран</option>
-              <option value="18-30">18-30 лет</option>
-              <option value="31-45">31-45 лет</option>
-              <option value="46-60">46-60 лет</option>
-              <option value="60+">60+ лет</option>
+            {AGE_OPTIONS.map(option => (
+                <option key={option.value} value={option.label}>
+                  {option.label}
+                </option>
+            ))}
             </select>
           </div>
 
@@ -90,12 +129,11 @@ function PersonalData() {
               onChange={(e) => setCountry(e.target.value)}
               className="profile__select"
             >
-              <option value="">Не выбрана</option>
-              <option value="ru">Россия</option>
-              <option value="kz">Казахстан</option>
-              <option value="by">Беларусь</option>
-              <option value="ge">Грузия</option>
-              <option value="tr">Турция</option>
+            {COUNTRY_OPTIONS.map(option => (
+              <option key={option.value} value={option.label}>
+                {option.label}
+              </option>
+            ))}
             </select>
           </div>
           
@@ -107,9 +145,11 @@ function PersonalData() {
               onChange={(e) => setLanguage(e.target.value)}
               className="profile__select"
             >
-              <option value="">Не выбран</option>
-              <option value="ру">русский</option>
-              <option value="en">english</option>
+            {LANGUAGE_OPTIONS.map(option => (
+              <option key={option.value} value={option.label}>
+                {option.label}
+              </option>
+            ))}
             </select>
           </div>
 
@@ -121,10 +161,11 @@ function PersonalData() {
               onChange={(e) => setCurrency(e.target.value)}
               className="profile__select"
             >
-              <option value="RUB">₽ Рубль</option>
-              <option value="USD">$ Доллар</option>
-              <option value="EUR">€ Евро</option>
-              <option value="TRY">₺ Лира</option>
+            {CURRENCY_OPTIONS.map(option => (
+              <option key={option.value} value={option.label}>
+                {option.label}
+              </option>
+            ))}
             </select>
           </div>
   
